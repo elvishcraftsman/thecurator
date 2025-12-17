58708542c6333824ca06ccd6a2cec908a8fb141b
Teledar <lmartin1920@outlook.com>
2025-12-17 12:00:21 -0500

    Decrease button height

--- patterns/dark-header.php
+++ patterns/dark-header.php
@@ -8,7 +8,7 @@
 <!-- wp:group {"align":"full","className":"curator-menu-row","style":{"spacing":{"blockGap":"0","padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"custom-curator-green","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignfull curator-menu-row has-custom-curator-green-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"className":"curator-menu-row","layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group curator-menu-row"><!-- wp:image {"lightbox":{"enabled":false},"width":"110px","height":"auto","sizeSlug":"large","linkDestination":"custom","style":{"layout":{"selfStretch":"fixed","flexSize":"110px"}}} -->
-<figure class="wp-block-image size-large is-resized"><a href="https://lynnm9.sg-host.com/"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Logo-for-green-background-use-1024x363.png" alt="The Curator" class="" style="width:110px;height:auto"/></a></figure>
+<figure class="wp-block-image size-large is-resized"><a href="https://lynnm9.sg-host.com/"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Logo-for-green-background-use-1024x363.png" alt="The Curator" style="width:110px;height:auto"/></a></figure>
 <!-- /wp:image -->
 
 <!-- wp:navigation {"textColor":"custom-white","backgroundColor":"custom-curator-green","showSubmenuIcon":false,"icon":"menu","overlayBackgroundColor":"custom-curator-green","overlayTextColor":"custom-white","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"}} /-->
@@ -17,8 +17,8 @@
 <!-- /wp:group -->
 
 <!-- wp:buttons {"style":{"layout":{"selfStretch":"fixed","flexSize":"126px"}},"layout":{"type":"flex","flexWrap":"nowrap","orientation":"horizontal","justifyContent":"left"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"white","textColor":"custom-curator-green","width":100,"className":"is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-curator-green"}}}}} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link has-custom-curator-green-color has-white-background-color has-text-color has-background has-link-color wp-element-button" href="http://eepurl.com/dlkQD9">Subscribe</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"white","textColor":"custom-curator-green","width":100,"className":"is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-curator-green"}}},"spacing":{"padding":{"left":"var:preset|spacing|40","right":"var:preset|spacing|40","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link has-custom-curator-green-color has-white-background-color has-text-color has-background has-link-color wp-element-button" href="http://eepurl.com/dlkQD9" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--40)">Subscribe</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group -->
